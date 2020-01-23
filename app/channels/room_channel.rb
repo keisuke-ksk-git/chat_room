20196351b847dd52050cc2884f560a1c0dc8b70b
keisuke-ksk-git <smoking.bar.ksk@gmail.com>
2020-01-23 20:23:28 +0900

    first commit

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,18 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    　 stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    
+
+  end
+
+  def speak(data)
+    message = Message.create!(content: data['message'])
+    template = ApplicationController.renderer.render(partial: 'messages/message', locals:{message: message})
+    ActionCable.server.broadcast 'room_channel', template
+    
+  end
+end
